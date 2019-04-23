bfa398246c1b6e2b2a30f0cf38b1a3b7f0bbf77f
enimgawxy <1480973357@qq.com>
2019-04-23 14:40:24 +0800

    dynamic display optimizing process

--- examples/part4/mlp.py
+++ examples/part4/mlp.py
@@ -40,28 +40,32 @@ if __name__ == '__main__':
     # Create session
     session = tg.Session()
 
+    # 设置动态绘制，不是训练完了才绘制结果
+    plt.ion()
+
     # Perform 100 gradient descent steps
     for step in range(100):
         J_value = session.run(J, feed_dict)
         if step % 10 == 0:
             print("Step:", step, " Loss:", J_value)
+
+            # 动态绘制结果图，你可以看到训练过程如何慢慢的拟合数据点
+            ax = plt.gca()
+            ax.set_title('Classifier')
+            ax.spines['right'].set_color('none')
+            ax.spines['top'].set_color('none')
+            plt.scatter(red_points[:, 0], red_points[:, 1], color='red')
+            plt.scatter(blue_points[:, 0], blue_points[:, 1], color='blue')
+
+            W_value = session.run(W)
+            b_value = session.run(b)
+            x_axis = np.linspace(-4, 4, 100)
+            y_axis = -W_value[0][0] / W_value[1][0] * x_axis - b_value[0] / W_value[1][0]
+            plt.plot(x_axis, y_axis, 'r')
+            plt.pause(0.1)
+
         session.run(minimization_op, feed_dict)
 
-    # Print final result
-    W_value = session.run(W)
-    print("Weight matrix:\n", W_value)
-    b_value = session.run(b)
-    print("Bias:\n", b_value)
-
-    # W_value = np.array([[1.27496197 - 1.77251219], [1.11820232 - 2.01586474]])
-    # b_value = np.array([-0.45274057 - 0.39071841])
-
-    # Plot a line y = -x
-    x_axis = np.linspace(-4, 4, 100)
-    y_axis = -W_value[0][0] / W_value[1][0] * x_axis - b_value[0] / W_value[1][0]
-    plt.plot(x_axis, y_axis)
-
-    # Add the red and blue points
-    plt.scatter(red_points[:, 0], red_points[:, 1], color='red')
-    plt.scatter(blue_points[:, 0], blue_points[:, 1], color='blue')
+    # 关闭动态绘制模式
+    plt.ioff()
     plt.show()

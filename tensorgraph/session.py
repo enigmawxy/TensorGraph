4e88da3ba0ef68c58be5951e711a1249885ada8f
enimgawxy <1480973357@qq.com>
2019-04-24 10:39:57 +0800

    增加调试信息

--- tensorgraph/session.py
+++ tensorgraph/session.py
@@ -8,7 +8,8 @@ class Session:
     """Represents a particular execution of a computational graph.
     """
 
-    def run(self, operation, feed_dict={}):
+    @staticmethod
+    def run(operation, feed_dict={}):
         """Computes the output of an operation
 
         Args:
@@ -16,34 +17,27 @@ class Session:
           feed_dict: A dictionary that maps placeholders to values for this session
         """
 
-        # Perform a post-order traversal of the graph to bring the nodes into the right order
-        nodes_postorder = traverse_postorder(operation)
+        nodes_post_order = traverse_post_order(operation)
 
-        # Iterate all nodes to determine their value
-        for node in nodes_postorder:
+        for node in nodes_post_order:
 
             if type(node) == placeholder:
-                # Set the node value to the placeholder value from feed_dict
                 node.output = feed_dict[node]
             elif type(node) == Variable:
-                # Set the node value to the variable's value attribute
                 node.output = node.value
-            else:  # Operation
-                # Get the input values for this operation from node_values
+            else:  
                 node.inputs = [input_node.output for input_node in node.input_nodes]
-
-                # Compute the output of this operation
                 node.output = node.compute(*node.inputs)
+                print("input: ", node.inputs)
 
-            # Convert lists to numpy arrays
+            print(type(node), node.output)
             if type(node.output) == list:
                 node.output = np.array(node.output)
 
-        # Return the requested node value
         return operation.output
 
 
-def traverse_postorder(operation):
+def traverse_post_order(operation):
     """Performs a post-order traversal, returning a list of nodes
     in the order in which they have to be computed
 
@@ -51,15 +45,18 @@ def traverse_postorder(operation):
        operation: The operation to start traversal at
     """
 
-    nodes_postorder = []
+    nodes_post_order = []
 
     def recurse(node):
         if isinstance(node, Operation):
             for input_node in node.input_nodes:
                 recurse(input_node)
+
         if node.name is not None:
             print(node.name)
-        nodes_postorder.append(node)
+
+        nodes_post_order.append(node)
 
     recurse(operation)
-    return nodes_postorder
+
+    return nodes_post_order
